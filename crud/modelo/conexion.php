9be56daec9616ad21e1ca285410d8d219f902acf
cibertp <cibertp462@gmail.com>
2018-03-30 20:13:24 -0500

    conectando a la base de dato

--- crud/modelo/conexion.php
+++ crud/modelo/conexion.php
@@ -1,10 +1,25 @@
 <?php
 
-class conexion 
+class conexion extends mysqli
 {
+  public function __construct(){
+    // private $servidor = 'localhost';//127.0.0.1
+    // private $user='root';
+    // private $pass='';
+    // private $batad='login';
 
+    parent:: __construct('localhost','root','','login');//accediendo a los metodos del padre mysqli
+
+        $this->query("SET NAME 'utf8';");
+        $this->connect_errno==true ? die('error con la conexion') : $x = 'conectado con exito';
+      //  echo $x; // este es un mensaje de prueba
+        unset($x);
+
+  }
 
 }
+
+$db = new conexion();
 
 
  ?>
---
+++ crud/vista/prueba.php
@@ -0,0 +1,20 @@
+<?php
+//include ('../modelo/conexion.php');
+require('../modelo/conexion.php'); //Utilizare por que es mas exigente
+
+$conex= new conexion(); // con esto ya sabemos que funciona  la perfecion la conexion a la base de dato
+
+
+
+
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+      <h1>hola mi querido cibert como estas</h1>
+  </body>
+</html>
